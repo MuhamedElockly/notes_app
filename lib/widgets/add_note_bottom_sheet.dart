cab0f80a617201b4de24de36d087c6789cd984c3
dell <elocklymuhamed@gmail.com>
2024-02-04 19:20:02 +0200

    add field to bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -4,15 +4,22 @@ import 'package:notes_app/widgets/custom_text_field%20copy.dart';
 class AddNoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: CustomTextField(
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          CustomTextField(
             hintText: 'Title',
           ),
-        ),
-      ],
+          SizedBox(
+            height: 20,
+          ),
+          CustomTextField(
+            hintText: 'Content',
+            maxLine: 4,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/custom_text_field copy.dart
+++ lib/widgets/custom_text_field copy.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  CustomTextField({this.hintText, this.onChanged});
+  CustomTextField({this.hintText, this.onChanged, this.maxLine = 1});
   String? hintText;
-
+  int maxLine;
   Function(String)? onChanged;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimaryColor,
       onChanged: onChanged,
+      maxLines: maxLine,
       decoration: InputDecoration(
-        
           hintText: hintText,
           hintStyle: TextStyle(color: kPrimaryColor),
-          
           border: buildBorder(),
           enabledBorder: buildBorder(),
           focusedBorder: buildBorder(
