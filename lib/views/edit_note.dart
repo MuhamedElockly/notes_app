68d189a93cd8d47769584784e9ec5e34891d9cb4
dell <elocklymuhamed@gmail.com>
2024-02-05 16:43:05 +0200

    add edit view

---
+++ lib/views/edit_note.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
+
+class EditNote extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
