51f139f335e897c9ce456339db4edd9d157768f5
dell <elocklymuhamed@gmail.com>
2024-02-07 22:39:14 +0200

    initialize hive

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
- const kPrimaryColor=Color(0xff62FCD7);
- const kSecendryColor=Color.fromARGB(255, 0, 0, 0);
+const kPrimaryColor = Color(0xff62FCD7);
+const kSecendryColor = Color.fromARGB(255, 0, 0, 0);
+const kNotesBox = 'notes_box';
