0e2261b558c8bc34c7cdfa69e3889787c1c2a313
dell <elocklymuhamed@gmail.com>
2024-02-18 21:30:50 +0200

    last update

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/cubit/cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field%20copy.dart';
 
@@ -72,8 +73,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
                     if (globalKey.currentState!.validate()) {
                       globalKey.currentState!.save();
 
-
-                       var noteModel=NoteModel(title: title!, subTitle: subTitle!, date: Date, color: color)
+                      var noteModel = NoteModel(
+                        title: title!,
+                        subTitle: subTitle!,
+                        date: DateTime.now().toString(),
+                        color: Colors.orange.value,
+                      );
+                      BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
                     } else {
                       autovalidateMode = AutovalidateMode.always;
                       setState(() {});
